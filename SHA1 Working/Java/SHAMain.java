fffe4777e9913964c3b3f97a7212d5a776f1b1d9
Siddhartha Rao <siddhartharao17@gmail.com>
2015-03-07 00:21:15 +0530

    Updated files on 07/03/2015 @ 12:21AM

--- SHA1 Working/Java/SHAMain.java
+++ SHA1 Working/Java/SHAMain.java
@@ -5,8 +5,14 @@ public class SHAMain
     //String testarray = "abc";
     //String resultarray = "A9 99 3E 36 47 06 81 6A BA 3E 25 71 78 50 C2 6C 9C D0 D8 9D";
     
-    String testarray = "a";
-    String resultarray = "86 F7 E4 37 FA A5 A7 FC E1 5D 1D DC B9 EA EA EA 37 76 67 B8";
+    //String testarray = "a";
+    //String resultarray = "86 F7 E4 37 FA A5 A7 FC E1 5D 1D DC B9 EA EA EA 37 76 67 B8";
+    
+    //String testarray = "abcdbcdecdefdefgefghfghighijhi";
+    //String resultarray = " f9537c23893d2014f365adf8ffe33b8eb0297ed1";
+    
+    String testarray = "0123456701234567012345670123456701234567012345670123456701234567";
+    String resultarray = "e0c094e867ef46c350ef54a7f59dd60bed92ae83";
     
     /* Reference created of class MySHA */
     MySHA context;
